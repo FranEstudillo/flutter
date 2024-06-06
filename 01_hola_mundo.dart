105671fd311a51f646ca8da1bd2f64217b550cb1
Francisco Estudillo <franciscoestudillo97@outlook.com>
2024-06-05 19:03:18 -0600

    05/06/2024

---
+++ 01_hola_mundo.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  String name = 'Fran';
+  int clave = 55013532;  
+  
+  print("""
+  Nombre: ${name.toUpperCase()} Clave: $clave  
+  """);
+  
+}
---
+++ 02_variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  //tipos de variables 
+  String name = 'Cyndaquil';
+  int id = 155;
+  bool isAlive = true;
+  dynamic locate = 'Casa';
+  
+  print("""
+  Nombre: $name
+  ID: $id
+  Registrado: $isAlive
+  Visto en: $locate
+  """);
+   
+}
